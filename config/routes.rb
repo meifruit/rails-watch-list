350159684bb67523b0e3b7829f7a77bb696f042f
Xuemei Huang <mei.huang9500@gmail.com>
2023-08-21 19:26:10 +0900

    fixed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+   root 'lists#index'
   resources :lists, only: [:index, :show, :new, :create, :destroy] do
    resources :bookmarks, only: [:new, :create]
   end
